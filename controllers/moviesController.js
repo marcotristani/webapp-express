0ab32918da1532f25d0b44117501623015761e3e
marcotristani <marco.tristani@hotmail.it>
2026-02-20 14:45:13 +0100

    flat: definita chiamata query db nella rotta show per ritornare il film richiesto

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -24,7 +24,26 @@ function index(req, res) {
 
 //funzione per rotta show
 function show(req, res) {
-  const id = req.params.id;
-  res.send("rotta show" + id);
+  //definisco query sql
+  const sqlMovies = "SELECT * FROM movies WHERE id = ? ";
+
+  //recupero id da parametro query API
+  const { id } = req.params;
+  //eseguo query al db
+  connection.query(sqlMovies, [id], (err, results) => {
+    //valuto se ritorna un errore dal db
+    if (err) return res.status(500).json({ error: "database not found" });
+    //valuto se non c'è il film richiesto
+    if (results.length === 0)
+      return res
+        .status(404)
+        .json({ error: "not found", message: "film non trovato" });
+
+    //costruisco l'oggetto di riposta con dati arrivati dal db
+    const movie = results[0];
+
+    //se non ci sono errori ritorna l'oggetto di riposta
+    res.json(movie);
+  });
 }
 module.exports = { index, show };
